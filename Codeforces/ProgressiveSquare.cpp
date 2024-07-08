bd1f7b9cf7ba814d4b82d167a7630842960e9445
Irakll <93161396+iraklidk@users.noreply.github.com>
2024-07-08 11:37:35 +0400

    Add files via upload

---
+++ Codeforces/ProgressiveSquare.cpp
@@ -0,0 +1,19 @@
+//IrakliDK
+//https://codeforces.com/problemset/problem/1955/B
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	int testcase;
+	cin >> testcase;
+
+	while (testcase--) {
+
+
+
+	}
+}
