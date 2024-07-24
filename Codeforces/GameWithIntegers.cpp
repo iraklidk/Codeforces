cb3f3e3b9ae1dd3b9dda1e22bdcfd40126f98db1
Irakll <93161396+iraklidk@users.noreply.github.com>
2024-07-25 00:48:25 +0400

    Add files via upload

---
+++ Codeforces/GameWithIntegers.cpp
@@ -0,0 +1,18 @@
+//IrakliDK
+//https://codeforces.com/problemset/problem/1899/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int tc; cin >> tc;
+	while (tc--) {
+
+		int n; cin >> n;
+
+		cout << (n % 3 == 0 ? "Second" : "First") << endl;
+		
+	}
+	return 0;
+}
