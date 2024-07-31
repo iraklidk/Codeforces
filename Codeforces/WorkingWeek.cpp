8b418e9aa2ad7a89b99d382ce030eebf2b001962
Irakli Dolbaia <93161396+iraklidk@users.noreply.github.com>
2024-08-01 01:44:17 +0400

    Add files via upload

---
+++ Codeforces/WorkingWeek.cpp
@@ -0,0 +1,31 @@
+//IrakliDK
+//https://codeforces.com/contest/1735/problem/A
+
+#include <bits/stdc++.h>
+#define no cout << "NO" << "\n"
+#define yes cout << "YES" << "\n"
+#define forr(i, n) for(int i=0;i<n;i++)
+#define sortVec(vec) sort(vec.begin(), vec.end())
+#define rsortVec(vec) sort(vec.rbegin(), vec.rend())
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<pair<int, int>> vpii;
+typedef vector<vector<ll>> vll;
+typedef vector<vector<int>> vvi;
+
+int main()
+{
+
+	int tc; cin >> tc;
+	while (tc--) {
+
+		int n; cin >> n;
+
+		cout << (n - 3) / 3 - 1 << endl;
+
+	}
+}
