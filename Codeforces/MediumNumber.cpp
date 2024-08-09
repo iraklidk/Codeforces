56d8ee59ad754fa8b0f352285d33316487da7194
Irakli Dolbaia <93161396+iraklidk@users.noreply.github.com>
2024-08-09 16:35:31 +0400

    Add files via upload

---
+++ Codeforces/MediumNumber.cpp
@@ -0,0 +1,39 @@
+//IrakliDK
+//https://codeforces.com/contest/1760/problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+#include <bits/stdc++.h>
+#define no cout << "NO" << "\n"
+#define yes cout << "YES" << "\n"
+#define forr(i, n) for(int i=0;i<n;i++)
+#define sortVec(vec) sort(vec.begin(), vec.end())
+#define rsortVec(vec) sort(vec.rbegin(), vec.rend())
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<long long> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<vector<bool>> vvb;
+typedef vector<pair<int, int>> vpii;
+
+int main()
+{
+	int tc; cin >> tc;
+	while (tc--) {
+
+		vi vec(3);
+		forr(i, 3) {
+			cin >> vec[i];
+		}
+
+		sortVec(vec);
+
+		cout << vec[1] << endl;
+
+	}
+}
