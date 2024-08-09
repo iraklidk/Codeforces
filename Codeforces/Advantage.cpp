3096dbde538cbaaae10ce6ba8f5c5e1eb5ef5a34
Irakli Dolbaia <93161396+iraklidk@users.noreply.github.com>
2024-08-09 16:34:52 +0400

    Add files via upload

---
+++ Codeforces/Advantage.cpp
@@ -0,0 +1,50 @@
+//IrakliDK
+//https://codeforces.com/contest/1760/problem/C
+
+#include <bits/stdc++.h>
+using namespace std;
+#include <bits/stdc++.h>
+#define no cout << "NO" << "\n"
+#define yes cout << "YES" << "\n"
+#define forr(i, n) for(int i=0;i<n;i++)
+#define sortVec(vec) sort(vec.begin(), vec.end())
+#define rsortVec(vec) sort(vec.rbegin(), vec.rend())
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<long long> vll;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<vector<bool>> vvb;
+typedef vector<pair<int, int>> vpii;
+
+int main()
+{
+	int tc; cin >> tc;
+	while (tc--) {
+
+		int n; cin >> n;
+		vi vec(n);
+		int maxima = 0;
+		forr(i, n) {
+			cin >> vec[i];
+			maxima = max(vec[i], maxima);
+		}
+
+		vi tmp = vec;
+
+		sortVec(tmp);
+
+		int k = tmp[n - 2];
+
+		forr(i, n) {
+			if (vec[i] == maxima) cout << maxima - k << " ";
+			else
+				cout << vec[i] - maxima << " ";
+		}
+
+	}
+}
