e2154d1d224d538a52ab6d9c70f3aad3a27cc5ab
Irakli Dolbaia <93161396+iraklidk@users.noreply.github.com>
2024-08-13 21:20:47 +0400

    Add files via upload

---
+++ Codeforces/SeatingInABus.cpp
@@ -0,0 +1,51 @@
+//IrakliDK
+//https://codeforces.com/contest/2000/problem/B
+
+#include <bits/stdc++.h>
+using namespace std;
+#include <bits/stdc++.h>
+#define no cout << "NO" << "\n"
+#define yes cout << "YES" << "\n"
+#define forr(i, n) for(int i=0;i<n;i++)
+#define sortVec(vec) sort(vec.begin(), vec.end())
+#define rsortVec(vec) sort(vec.rbegin(), vec.rend())
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef vector<char> vc;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvll;
+typedef vector<vector<bool>> vvb;
+typedef vector<pair<ll, ll>> vpll;
+typedef vector<pair<int, int>> vpii;
+
+
+int main()
+{
+	int tc; cin >> tc;
+
+	while (tc--) {
+
+		int n; cin >> n;
+		vi vec(n);
+		map<int, int> mp;
+		forr(i, n) {
+			cin >> vec[i];
+		}
+
+		string ans = "YES";
+		mp[vec[0]]++;
+
+		for (int i = 1; i < n; i++) {
+			if (mp[vec[i] + 1] == 0 && mp[vec[i] - 1] == 0) {
+				ans = "NO";
+			}
+			mp[vec[i]]++;
+		}
+
+		cout << ans << endl;
+	}
+}
